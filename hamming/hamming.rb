39bb915a516017929a22430733e75a2ffda85a3e
Pavel Razuvayev <prx124@my.utsa.edu>
2015-04-12 15:27:38 -0500

    Refactor hamming.rb

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -3,14 +3,10 @@ class Hamming
 
     str_length = str_a.chars.length
 
-    index = 0
     match = 0
 
-    str_length.times do
-      if str_a.chars.at(index) == str_b.chars.at(index)
-        match += 1
-      end
-      index += 1
+    str_length.times do |index|
+      match += 1 if str_a.chars.at(index) == str_b.chars.at(index)    
     end
 
     return str_length - match
