1fed6c8d88dea3b75f37aaf8a8c89c82bc52d8dc
Pavel Razuvayev <pavel.razuvayev@gmail.com>
2015-04-22 20:49:33 -0500

    more readable name generator for robot-name

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -7,8 +7,9 @@ class Robot
 
   def name_generator
     # random name generated with 2 letter prefix and 3 integers
-    (0...2).map { (65 + rand(26)).chr }.join.concat(
-    (0...3).map { rand(9) }.join)
+    #(0...2).map { (65 + rand(26)).chr }.join.concat(
+    #(0...3).map { rand(9) }.join)
+    (('A'..'Z').to_a.shuffle[0...2].join + (0..9).to_a.shuffle[0...3].join).to_s
   end
 
   def reset
